ec53281ae7300b733890f4c9dbcfcd3668de10c5
HeProfessor <hefacebook100@gmail.com>
2024-11-19 15:54:20 -0300

    Criãção do config

---
+++ config/database.js
@@ -0,0 +1,20 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize({
+    host: "localhost",
+    dialect: "sqlite",
+    storage: "./bancoVenturus.db"
+});
+
+const testConnection = async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("Banco de dados no ar");
+    } catch (err) {
+        console.error("Não foi possível conectar ao banco de dados:", err);
+    }
+};
+
+testConnection();
+
+export default sequelize;
